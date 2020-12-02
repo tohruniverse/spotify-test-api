14fa46f96e8e210ea19c889e3267d74e84075bce
tohruniverse <starscreamvn@gmail.com>
2020-12-02 21:13:07 +0700

    Initialize

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
+import "./App.css";
+import Login from "./Login";
+
+const s = new SpotifyWebApi();
+
+function App() {
+  const [{ token }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      s.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      s.getPlaylist("37i9dQZF1E398CVoWRYFri").then((response) => {
+        console.log('response', response)
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      }
+      );
+
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={s} />}
+      <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1E35skqQToH4rm" width="450" height="380" frameborder="3" allowtransparency="false" allow="encrypted-media"></iframe>
+    </div>
+  );
+}
+
+export default App;
